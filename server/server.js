506c438b5895fff82755c8c1e4875f5b23f3a31b
Farhan Uz Zaman <51168503+Farhan-Uz-Zaman@users.noreply.github.com>
2020-09-29 21:55:42 +0600

    server file

--- server/server.js
+++ server/server.js
@@ -88,5 +88,5 @@ app.delete("/api/Restaurants/:id", async (req, res) => {
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
-    console.log(`server is up and fuck you!${port}`);
+    console.log(`server is up and Thank you!${port}`);
 });
